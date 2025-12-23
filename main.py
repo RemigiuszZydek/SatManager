f8f3ed5e569519e6bb810c80b8791ae8821823c7
remo <zyduremik@gmail.com>
2025-12-23 18:01:16 +0100

    auth dziala. Teraz musze to zrobic lepiej. Ładnie podzielic kod, dodac role najlepiej jako enumy ktore sie bedzie dodawac przy tworzeniu konta i zrobic clean code architecture

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Annotated
 from sqlalchemy.orm import Session 
 from .users import models
 from .database.database import engine, SessionLocal, get_db
-from .auth.auth import router as auth_route
+from .auth.auth import router as auth_route, get_current_user
 
 app = FastAPI()
 app.include_router(auth_route)
@@ -11,9 +11,10 @@ app.include_router(auth_route)
 models.Base.metadata.create_all(bind=engine)
 
 db_dependency = Annotated[Session, Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
 
 @app.get("/", status_code=status.HTTP_200_OK)
-async def user(user: None, db: db_dependency):
+async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication failed")
     return {"User":user}
