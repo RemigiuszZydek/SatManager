ed4e3ceb96baf2f6cf80c0f6e81f651d38843a50
remo <zyduremik@gmail.com>
2025-12-29 20:58:42 +0100

    początki testów

--- main.py
+++ main.py
@@ -15,14 +15,19 @@ app.include_router(auth_route)
 app.include_router(test_role_route)
 app.include_router(tasks_route)
 
-models.Base.metadata.create_all(bind=engine)
+def init_db():
+    models.Base.metadata.create_all(bind=engine)
+    seed_roles()
 
 db_dependency = Annotated[Session, Depends(get_db)]
 user_dependency = Annotated[dict, Depends(get_current_user)]
-seed_roles()
+
 
 @app.get("/", status_code=status.HTTP_200_OK)
 async def user(user: user_dependency, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication failed")
     return {"User":user}
+
+if __name__ == "__main__":
+    init_db()
